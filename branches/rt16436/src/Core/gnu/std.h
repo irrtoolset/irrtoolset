051702518bde52d69b5814073b316230dadf9b14
shane <shane@b0b9f8c7-bce7-4f2d-a5ae-482af58f070a>
2006-11-21 11:37:01 +0000

    Merge portability patches from NetBSD, series a.

--- branches/rt16436/src/Core/gnu/std.h
+++ branches/rt16436/src/Core/gnu/std.h
@@ -19,7 +19,7 @@ Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 #ifndef _std_h
 #define _std_h 1
 
-#include <_G_config.h>
+#include <config.h>
 #include <cstddef>
 #include <cstdlib>
 #include <cstring>
@@ -31,7 +31,7 @@ Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 extern "C" {
 #ifndef HAVE_STRINGS_H
-int strcasecmp _G_ARGS((const char*, const char*));
+int strcasecmp __P((const char*, const char*));
 #endif // HAVE_STRINGS_H
 }
 
