c2c0080702764f3c57f687e9ef64db58ee99a57e
shane <shane@b0b9f8c7-bce7-4f2d-a5ae-482af58f070a>
2007-02-09 19:46:31 +0000

    Numerous compilation fixes. See RT ticket 16436 for details.

--- trunk/src/normalform/RadixSet.cc
+++ trunk/src/normalform/RadixSet.cc
@@ -878,7 +878,7 @@ ostream& operator<<(ostream& o, const RadixSet &set) {
       	       need_comma = true;
       	    o << int2quad(buffer, addr) << "/" << leng << "^" << n << "-" << m;
       	 } // end of for loop
-      } else { // not a compressed point
+      } else { // not a compressed print
        	 RadixSet::PrefixIterator itr(&set);
 	       for (bool flag = itr.first(addr, leng); flag; flag = itr.next(addr, leng)) {
            if (need_comma)
