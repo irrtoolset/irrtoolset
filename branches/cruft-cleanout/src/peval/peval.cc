e1dfd346de513e23b855d551a747efbb45fe4ec8
nick <nick@b0b9f8c7-bce7-4f2d-a5ae-482af58f070a>
2009-06-12 11:55:09 +0000

    Change global variable "filter" in peval.cc to be "irrfilter" in order to prevent a clash between the globally defined "filter" variable in -ltermcap on Solaris.  Naughty Solaris for using "filter" as a global variable name in such a common library.

--- branches/cruft-cleanout/src/peval/peval.cc
+++ branches/cruft-cleanout/src/peval/peval.cc
@@ -103,18 +103,18 @@ const int SIZE = 8*1024;
 char base[SIZE] = "peval: ";
 char temp[SIZE];
 char safe_base[SIZE];
-char *filter;
+char *irrfilter;
 char *cut;
 
 void evaluate() {
    if (opt_expand & EXPAND_ASSets)
       regexp_nf::expandASSets();
 
-   strcat(filter, "\n\n");
+   strcat(irrfilter, "\n\n");
    // Was: safe_base = base;
    memcpy(safe_base, base, SIZE);
 
-   cut = strstr(filter, "afi");
+   cut = strstr(irrfilter, "afi");
    if (cut && isspace(*(cut+3))) {
      strcat (temp, "mp-");
      strcat (temp, base);
@@ -207,7 +207,7 @@ int skipAll(char *dst, char *key, char *nextArg) {
 int expression(char *dst, char *key, char *nextArg) {
    if (!nextArg) return 0;
 
-   strcpy(filter, nextArg);
+   strcpy(irrfilter, nextArg);
    return 1;
 }
 
@@ -266,7 +266,7 @@ void init_and_set_options (int argc, char **argv, char **envp) {
    // the first one is the expression
    switch (argc) {
    case 2:
-      strcpy(filter, argv[1]);
+      strcpy(irrfilter, argv[1]);
       break;
    case 1:
       break;
@@ -275,7 +275,7 @@ void init_and_set_options (int argc, char **argv, char **envp) {
       exit(-1);
    }
 
-   if (*filter) {
+   if (*irrfilter) {
       evaluate();
       exit(0);
    }
@@ -314,7 +314,7 @@ rl_gets (char *prompt)
 
 int main (int argc, char **argv, char **envp) {
    int newSize = strlen(base);
-   filter = base + newSize;
+   irrfilter = base + newSize;
    newSize = SIZE - newSize - 3;
 
    schema.initialize();
@@ -325,12 +325,12 @@ int main (int argc, char **argv, char **envp) {
       char *line = rl_gets(opt_prompt);
       if (!line)
 	 break;
-      strncpy(filter, line, newSize);
+      strncpy(irrfilter, line, newSize);
       evaluate();
 #else // HAVE_READLINE
       if (opt_prompt)
 	 cout << opt_prompt;
-      if (cin.getline(filter, newSize))
+      if (cin.getline(irrfilter, newSize))
 	 evaluate();
       else
 	 break;
