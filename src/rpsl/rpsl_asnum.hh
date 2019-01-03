5889b2ffd1e176a7e4a12746a4b56936e5d3488a
Aliaksei Sheshka <afpd@yandex.ru>
2019-01-02 21:51:52 -0500

    Fix for integer overflow #51

--- src/rpsl/rpsl_asnum.hh
+++ src/rpsl/rpsl_asnum.hh
@@ -6,22 +6,22 @@ extern bool opt_asdot;
 inline int asnum_string(char *buf, unsigned int asno)
 {
    if (asno > 65535 && opt_asdot)
-      return sprintf(buf, "AS%d.%d", asno>>16, asno&0xffff);
+      return sprintf(buf, "AS%u.%u", asno>>16, asno&0xffff);
    else
-      return sprintf(buf, "AS%d", asno);
+      return sprintf(buf, "AS%u", asno);
 }
 
 inline int asnum_string_dot(char *buf, unsigned int asno)
 {
    if (asno > 65535)
-      return sprintf(buf, "AS%d.%d", asno>>16, asno&0xffff);
+      return sprintf(buf, "AS%u.%u", asno>>16, asno&0xffff);
    else
-      return sprintf(buf, "AS%d", asno);
+      return sprintf(buf, "AS%u", asno);
 }
 
 inline int asnum_string_plain(char *buf, unsigned int asno)
 {
-   return sprintf(buf, "AS%d", asno);
+   return sprintf(buf, "AS%u", asno);
 }
 
 #endif
